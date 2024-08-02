69281775f06d3483dd1fca0943001f3898368cef
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2024-08-02 11:17:37 +0900

    컴포넌트별 CSS 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Button from "./Button";
+import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+
   return (
     <div className="App">
-      <h1>Welcome back!</h1>
+      <h1 className={styles.title}>Welcome back!</h1>
       <Button text={"Continue"} />
     </div>
   );
